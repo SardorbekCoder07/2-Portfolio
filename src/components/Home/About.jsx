98a253d065e0db58ee0bd9adedf8ea5d89470ed0
Sardorbek <sardorbekcoderjon@gmail.com>
2023-08-25 09:35:36 +0500

    Rasm qoshildi

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -4,10 +4,8 @@ import LaptopImg from "../../assets/home-main.svg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
   AiFillInstagram,
 } from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
 
 
 const About = () => {
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -5,13 +5,13 @@ import { AiOutlineDownload } from "react-icons/ai";
 
 import Particle from '../components/Particle'
 import pdf from "../assets/sajib.pdf"
+import rezyume from "../assets/projects/rezyume.png"
 
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const resumeLink = `https://raw.githubusercontent.com/19sajib/portfolio/main/src/assets/sajib.pdf`
 
 
 const Resume = () => {
@@ -39,7 +39,8 @@ const Resume = () => {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
+          <img src={rezyume} alt="/"  className='w-50'/>
+          <Document  className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
