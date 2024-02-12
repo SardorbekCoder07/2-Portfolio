e73b22fa1b5c66aea343d348848feb425dd7c385
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-02-13 00:22:04 +0300

    bb

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,6 +6,7 @@ import pg from "../assets/projects/screen2.jpg";
 import project from "../assets/projects/screen5.jpg";
 import lift from "../assets/projects/screen4.jpg";
 import kickstart from "../assets/projects/screen6.jpg";
+import screen7 from '../assets/projects/screen7.png'
 
 const Projects = () => {
   return (
@@ -61,6 +62,16 @@ const Projects = () => {
               description="This personal website does not have an API server built with programming languages ​​such as HTML.CSS and Javascript. Data is retrieved from the API server and displayed in a user-friendly interface built using React and styled components for a seamless experience."
               ghLink="https://github.com/SardorbekCoder07/FolioPortfolio"
               demoLink="https://sardorbekcoderjon.netlify.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={screen7}
+              isBlog={false}
+              title="Ryadhs Bus web page"
+              description="We made this site for Saudi Arabia. We have made this site to regulate the main Bus traffic and to bring relief to Saudi citizens. We made this site together with our team. This site is perfectly built in React JS. A very beautiful design has been created based on tailwind, and the responsive design is amazingThis personal website does not have an API server built with programming languages ​​such as HTML.CSS and Javascript. Data is retrieved from the API server and displayed in a user-friendly interface built using React and styled components for a seamless experience."
+              ghLink="https://github.com/Behruz1227/Saudi-project"
+              demoLink="http://xconnect.uz/"
             />
           </Col>
         </Row>
