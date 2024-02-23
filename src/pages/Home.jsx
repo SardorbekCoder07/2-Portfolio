a59fc0d5f9d8a436ce20751cc00246b47a54b4ed
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-02-23 12:47:25 +0300

    Junior suzi olib tawlandi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Junior Sardorbek Sayfullayev</strong>
+                <strong className="main-name">  Sardorbek Sayfullayev</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
