809ba86dbd57689ef2a129eeeb3a14bb80f75aad
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-06-03 10:17:20 +0300

    haa

--- src/components/Contact/Social.jsx
+++ src/components/Contact/Social.jsx
@@ -1,60 +1,56 @@
-import React from 'react'
+import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import {
-  AiFillGithub,
-  AiFillInstagram,
-} from "react-icons/ai";
+import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
 import { FaTelegram } from "react-icons/fa";
-import './Social.css'
+import "./Social.css";
 
 const Social = () => {
-  return (
-    <Container style={{padding: '30px'}}>
-    <Row>
-              <Col md={12} className="contact-social">
-                <div className='contact-text'>
-                <h1>FIND ME ON</h1>
-                <p>
-                Please don't hesitate to reach out to me and <span className="yellow">connect.</span>
-                </p>
-                </div>
-                <ul className="contact-social-links">
-                  <li className="contact-icons">
-                    <a
-                      href="https://github.com/SardorbekCoder07"
-                      target="_blank"
-                      rel="noreferrer"
-                      className="icon-color  contact-social-icons"
-                    >
-                      <AiFillGithub />
+	return (
+		<Container style={{ padding: "30px" }}>
+			<Row>
+				<Col md={12} className="contact-social">
+					<div className="contact-text">
+						<h1>FIND ME ON</h1>
+						<p>
+							Please don't hesitate to reach out to me and <span className="yellow">connect.</span>
+						</p>
+					</div>
+					<ul className="contact-social-links">
+						<li className="contact-icons">
+							<a
+								href="https://github.com/SardorbekCoder07"
+								target="_blank"
+								rel="noreferrer"
+								className="icon-color  contact-social-icons"
+							>
+								<AiFillGithub />
+							</a>
+						</li>
+						<li className="contact-icons">
+							<a
+								href="https://t.me/Tuit_ki_student"
+								target="_blank"
+								rel="noreferrer"
+								className="icon-color  contact-social-icons"
+							>
+								<FaTelegram />
+							</a>
+						</li>
+						<li className="contact-icons">
+							<a
+								href="https://www.instagram.com/sardorbek_abdurashidovich/"
+								target="_blank"
+								rel="noreferrer"
+								className="icon-color contact-social-icons"
+							>
+								<AiFillInstagram />
+							</a>
+						</li>
+					</ul>
+				</Col>
+			</Row>
+		</Container>
+	);
+};
 
-                    </a>
-                  </li>
-                  <li className="contact-icons">
-                    <a
-                      href="https://t.me/Tuit_ki_student"
-                      target="_blank"
-                      rel="noreferrer"
-                      className="icon-color  contact-social-icons"
-                    >
-                      <FaTelegram/>
-                    </a>
-                  </li>
-                  <li className="contact-icons">
-                    <a
-                      href="https://www.instagram.com/sardorbek_abdurashidovich/"
-                      target="_blank"
-                      rel="noreferrer"
-                      className="icon-color contact-social-icons"
-                    >
-                      <AiFillInstagram />
-                    </a>
-                  </li>
-                </ul>
-              </Col>
-            </Row>
-          </Container>
-  )
-}
-
-export default Social
+export default Social;
